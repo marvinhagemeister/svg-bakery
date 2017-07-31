783a3eac0a2826b57652730df48b355c4e50330d
Marvin Hagemeister <marvin@marvinhagemeister.de>
2017-07-31 23:31:41 +0200

    WIP

---
+++ src/__tests__/spriter2.spec.ts
@@ -0,0 +1,76 @@
+import { assert as t } from "chai";
+import * as path from "path";
+import { readFile } from "nicer-fs";
+import { render, ast2VNode, SVGAst } from "../spriter2";
+
+async function getFixtures(name: string) {
+  const root = path.join(__dirname, "fixtures2");
+  const input = await readFile(path.join(root, name, "input.svg"), "utf-8");
+  const output = await readFile(path.join(root, name, "output.svg"), "utf-8");
+  return { input, output };
+}
+
+describe("render", () => {
+  it("should render simple", async () => {
+    const { input, output } = await getFixtures("simple");
+    const res = await render([input], { mode: "symbol" });
+    t.equal(res, output);
+  });
+});
+
+describe.only("ast2VNode", () => {
+  it("should ", () => {
+    const ast: SVGAst = {
+      svg: {
+        $: {
+          width: "100",
+          height: "100",
+        },
+        g: [
+          {
+            title: ["layer1"],
+            ellipse: [],
+          },
+        ],
+      },
+    };
+
+    const res = ast2VNode(ast);
+    console.log("RES", res);
+
+    t.deepEqual(res, {
+      tag: "svg",
+      props: {
+        width: "100",
+        height: "100",
+      },
+      children: [
+        {
+          tag: "g",
+          props: {},
+          children: [
+            {
+              tag: "title",
+              props: {},
+              children: ["Layer 1"],
+            },
+            {
+              tag: "ellipse",
+              props: {
+                ry: "21",
+                rx: "19",
+                id: "svg_1",
+                cy: "30",
+                cx: "29",
+                "stroke-width": "5",
+                stroke: "#000000",
+                fill: "#FF0000",
+              },
+              children: [],
+            },
+          ],
+        },
+      ],
+    });
+  });
+});
--- src/spriter2.ts
+++ src/spriter2.ts
@@ -1,3 +1,14 @@
+import { parseString } from "xml2js";
+
+export function parseXml(content: string): Promise<SVGAst> {
+  return new Promise((resolve, reject) => {
+    parseString(
+      content,
+      (err, result) => (err !== null ? reject(err) : resolve(result)),
+    );
+  });
+}
+
 export interface Options {
   mode: "symbol";
   addNamespace?: boolean;
@@ -8,8 +19,103 @@ const defaultOpts: Options = {
   addNamespace: true,
 };
 
-export function render(files: string[], opts: Options) {
-  let out: string = '<?xml version="1.0" encoding="utf-8"?><svg';
+export interface VNode {
+  tag: string;
+  props: Record<string, string | number>;
+  children: VNode[] | string[];
+}
+
+export function createVNode(tag: string): VNode {
+  return {
+    tag: "svg",
+    props: {},
+    children: [],
+  };
+}
+
+export interface SVGAst {
+  svg: Ast;
+}
+
+export interface Ast {
+  $?: Record<string, string>;
+  title?: string[];
+  ellipse?: Ast[];
+  polygon?: Ast[];
+  g?: Ast[];
+  [index: string]: any;
+}
+
+export function isRootAst(ast: SVGAst | Ast): ast is SVGAst {
+  return (ast as any).svg !== undefined;
+}
+
+export function ast2VNode(ast: SVGAst | Record<string, Ast>): VNode {
+  const el = Object.keys(ast)[0];
+  const tag = isRootAst(ast) ? "symbol" : el;
+  const obj: Ast = (ast as any)[el];
+
+  const vnode = createVNode(tag);
+  if (obj.$ !== undefined) {
+    vnode.props = obj.$;
+  }
+
+  vnode.children = parseChildren(obj);
+
+  return vnode;
+}
+
+export function parseChildren(obj: Ast): VNode[] {
+  return Object.keys(obj).filter(key => key !== "$").reduce((prev, key) => {
+    const next: Ast | Ast[] = (obj as any)[key];
+    const node = createVNode(key);
+
+    // const children = next.map(it);
+    if (Array.isArray(next)) {
+      prev = [...prev, ...parseChildren(next)];
+    } else {
+      console.log("foo");
+    }
+
+    prev.push(node);
+    return prev;
+  }, []);
+}
+
+export async function buildSvg(contents: string[]): Promise<VNode> {
+  const parsed = await Promise.all(contents.map(content => parseXml(content)));
+
+  const out = createVNode("svg");
+  out.props = {
+    width: 1000,
+    height: 1000,
+  };
+
+  out.children = parsed.map(file => {
+    // We need to remove the root svg node.
+    let id = file.svg.$.id;
+    const nodes = Object.keys(file.svg).filter(key => key !== "$").map(key => {
+      const vnode = createVNode(key);
+
+      if (id === undefined && file.svg.title !== undefined) {
+        id = file.svg.title[0];
+      }
+      vnode.props.id = id;
+
+      if (Array.isArray((file as any)[key])) {
+        vnode.children = (file as any)[key];
+      }
+      return vnode;
+    });
+
+    return [].concat.apply([], nodes);
+  });
+
+  return out;
+}
+
+export async function render(files: string[], opts: Options) {
+  let out: string = '<?xml version="1.0" encoding="utf-8"?>\n<svg';
 
   if (opts.addNamespace) {
     out +=
@@ -17,7 +123,19 @@ export function render(files: string[], opts: Options) {
       'xmlns: xlink = "http://www.w3.org/1999/xlink"';
   }
 
+  const parsed = await Promise.all(files.map(file => parseXml(file)));
+
   if (opts.mode === "symbol") {
+    out += parsed
+      .map(xml => {
+        console.log(xml as any);
+
+        let content = '<symbol viewBox="23 20 50 50" id="allgefahren">';
+
+        content += "</symbol>";
+        return content;
+      })
+      .join("");
   }
 
   return out;
